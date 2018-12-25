8f659637be8cf4ac9a3c37cdb810a897d2e66ba4
alex-taxiera <alex.taxiera@gmail.com>
2018-12-24 22:35:32 -0500

    added command class and example

---
+++ data/commands/ping.js
@@ -0,0 +1,23 @@
+const Command = require('../customModules/Command.js')
+
+const data = {
+  name: 'ping',
+  description: "Check if the bot is online, display the bot's ping in ms, and view the amount of servers the bot's in",
+  action: (client, msg, knex) => {
+    const shard = msg.channel.type ? client.shards.get(0) : msg.channel.guild.shard
+    const clientLatency = Date.now() - msg.timestamp
+    return {
+      embed: {
+        title: 'Bot Status',
+        description: `Pong! Client: **${clientLatency}ms** API: **${shard.latency}ms** | Servers: **${client.guilds.filter(g => g.shard.id === shard.id).length}**`,
+        color: clientLatency > 200 && clientLatency < 300 ? 16776960 : clientLatency > 300 ? 15933733 : 111111,
+        author: {
+          name: 'Ping',
+          icon_url: links.ping
+        }
+      }
+    }
+  }
+}
+
+module.exports = new Command(data)
