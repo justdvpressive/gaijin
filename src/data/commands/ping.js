fbe05877f35b53aac02bc63a4bf27745d3a0364b
alex-taxiera <alex.taxiera@gmail.com>
2019-01-10 15:46:02 -0500

    file reorg and command handler overhaul

--- src/data/commands/ping.js
+++ src/data/commands/ping.js
@@ -1,9 +1,9 @@
-const Command = require('../customModules/Command.js')
+const Command = require('../../modules/command')
 
 const data = {
   name: 'ping',
-  description: "Check if the bot is online, display the bot's ping in ms, and view the amount of servers the bot's in",
-  action: (client, msg, knex) => {
+  desc: 'Check if the bot is online, display the bot\'s ping in ms, and view the amount of servers the bot\'s in',
+  action: ({ client, msg }) => {
     const shard = msg.channel.type ? client.shards.get(0) : msg.channel.guild.shard
     const clientLatency = Date.now() - msg.timestamp
     return {
@@ -13,7 +13,7 @@ const data = {
         color: clientLatency > 200 && clientLatency < 300 ? 16776960 : clientLatency > 300 ? 15933733 : 111111,
         author: {
           name: 'Ping',
-          icon_url: links.ping
+          icon_url: ''
         }
       }
     }
