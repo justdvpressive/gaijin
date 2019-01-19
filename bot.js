f9c875241147e91cad728f6ad06511aa63f14faf
alex-taxiera <alex.taxiera@gmail.com>
2019-01-18 20:18:12 -0500

    clean main

--- bot.js
+++ bot.js
@@ -1,19 +1,16 @@
 const Agent = require('./src/agent')
+
 const {
   TOKEN,
   DATABASE_URL,
-  TABLE,
   DBL_TOKEN,
-  CONNECT_LIMIT,
   PREFIX
 } = process.env
 
 const agent = new Agent(TOKEN, {
-  connectionURL: DATABASE_URL + '?ssl=true',
-  tableName: TABLE
+  connectionURL: DATABASE_URL
 }, {
   prefix: PREFIX,
-  connectRetryLimit: CONNECT_LIMIT,
   dblToken: DBL_TOKEN
 })
 
