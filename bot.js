4562356efc3c5750def2261e57b04be373270f31
alex-taxiera <alex.taxiera@gmail.com>
2019-02-05 15:28:26 -0500

    put placeholder dbl widget in links.json

--- bot.js
+++ bot.js
@@ -7,11 +7,16 @@ const {
   PREFIX
 } = process.env
 
+const {
+  dblWidget
+} = require('./src/data/links.json')
+
 const agent = new Agent(TOKEN, {
   connectionURL: DATABASE_URL
 }, {
   prefix: PREFIX,
-  dblToken: DBL_TOKEN
+  dblToken: DBL_TOKEN,
+  dblWidget
 })
 
 agent.connect()
